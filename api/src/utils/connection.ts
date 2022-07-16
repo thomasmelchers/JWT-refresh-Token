482e357faeeab933ad86bfc1bb47189b5c2f9172
Thomas Melchers <thomas.melchers@gmail.com>
2022-07-16 14:34:55 +0200

    connection with db

---
+++ api/src/utils/connection.ts
@@ -0,0 +1,18 @@
+import mongoose from 'mongoose';
+import config from 'config';
+import logger from './logger'
+
+export const connect = async () => {
+    const databaseUrl = config.get<string>('dbURL');
+
+    try {
+        await mongoose.connect(databaseUrl);
+        logger.info('Connected to DB')
+    } catch (error: any) {
+        logger.error('Could not connect to DB')
+        // Method used to end the process which is running
+        // if param = 0 : exit without failure
+        // if param = 1 : exit with failure 
+        process.exit(1);
+    }
+}
