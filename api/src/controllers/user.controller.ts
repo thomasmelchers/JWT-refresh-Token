6a1707f1287ea56b17e2a82d9e0bbd1c4bf5f826
Thomas Melchers <thomas.melchers@gmail.com>
2022-07-16 14:36:54 +0200

    user controller - create and get userHandler

---
+++ api/src/controllers/user.controller.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from "express";
+import logger from "../utils/logger";
+import {omit} from "lodash";
+import { createUser, getUser } from "../services/user.services";
+import { IUser } from "../models/interfaces/user.interface";
+
+
+
+export const createUserHandler = async (req: Request, res: Response) => {
+    try {
+
+        const user = await createUser(req.body);
+        return res.status(201).send(user);
+
+    } catch (err: any) {
+
+        res.status(403).send(err.message);
+
+    }
+}
+
+export const getUserHandler = async (req: Request, res: Response) => {
+    try {
+
+        const { password, email } = req.body
+        const user = await getUser({ email, password });
+        res.status(200).send(user);
+
+    } catch (err: any) {
+
+        logger.error(err);
+        res.status(401).send(err.message)
+
+    }
+}
